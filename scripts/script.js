76a6bf321178a04c3809acba8d6d5ebab92cc38e
Houssam Benlagha <houssambenlagha0@gmail.com>
2023-10-24 17:05:11 +0100

    menu animaiton is done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.js-header__button');
 const icon = document.querySelector('.js-header__icon');
+const nav = document.querySelector('.js-header__nav');
 let clicked = false;
 
 btn.addEventListener('click', () => {
@@ -7,11 +8,14 @@ btn.addEventListener('click', () => {
     icon.style.background = 'transparent';
     icon.classList.add('header__cancel');
     icon.classList.remove('header__icon');
+    nav.style.animation = 'slideIn 0.5s forwards';
+    nav.style.visibility = 'visible';
     clicked = true;
     }else {
         icon.style.background = 'white';
     icon.classList.add('header__icon');
     icon.classList.remove('header__cancel');
+    nav.style.animation = 'slideOut 0.5s forwards';
     clicked = false;
     }
 });
